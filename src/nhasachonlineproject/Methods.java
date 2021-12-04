d75380fa8ce93039baba1e42950c59b245bcf43d
Huy Phạm <phamduchuy@gmail.com>
2021-12-04 20:09:35 +0700

    Nha Sach

--- src/nhasachonlineproject/Methods.java
+++ src/nhasachonlineproject/Methods.java
@@ -1,5 +1,7 @@
 package nhasachonlineproject;
 
+import java.util.regex.Pattern;
+
 public class Methods {
     public static boolean checkNum(String yourstr){
         if (yourstr == null || yourstr.length() == 0)
@@ -19,5 +21,14 @@ public class Methods {
             }
         }
         return false;
+    }
+    
+    public static boolean isEmail(String yourmail)
+    {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\." + "[a-zA-Z0-9_+&*-]+)*@" + "(?:[a-zA-Z0-9-]+\\.)+[a-z" + "A-Z]{2,7}$";                         
+        Pattern pat = Pattern.compile(emailRegex);
+        if (yourmail == null)
+            return false;
+        return pat.matcher(yourmail).matches();
     }
 }
